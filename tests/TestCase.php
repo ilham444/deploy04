1f093ef6287747c54e17273b590cf323be454744
ilham444 <2206020@itg.ac.id>
2025-07-07 19:27:28 +0700

    Fix: Add missing TestCase and configure autoloader

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,6 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    // Biasanya kelas ini bisa digunakan untuk menambahkan helper methods
+    // untuk semua test Anda, tapi untuk sekarang biarkan kosong.
 }
